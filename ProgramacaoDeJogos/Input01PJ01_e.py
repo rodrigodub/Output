2f68ea9637dae419801fafacba97b17c3db5999f
Rodrigo <nobrega@gmail.com>
2015-06-02 19:37:27 +0800

    Programação de Jogos 1, animação
    # v1.1
    # 20150602

---
+++ ProgramacaoDeJogos/Input01PJ01_e.py
@@ -0,0 +1,45 @@
+# INPUT
+# Programacao de Jogos
+
+# Implementacao em Python by RN
+# com uso da biblioteca CURSES para 
+# manipulacao da janela de texto (terminal)
+
+# resource: https://docs.python.org/3.4/howto/curses.html#curses-howto
+# resource: https://docs.python.org/3.4/library/curses.html?highlight=curses#module-curses
+
+# v1.1
+# 20150602
+
+import time, curses
+from curses import wrapper
+
+
+def drawinsect(stdscr, posy, posx):
+	# draws insect
+	stdscr.addstr(posy, posx, ' )))')
+	stdscr.addstr(posy+1, posx, ' OOO<')
+	stdscr.addstr(posy+2, posx, ' )))')
+	stdscr.refresh()
+	time.sleep(0.2)
+	stdscr.addstr(posy, posx, ' (((')
+	stdscr.addstr(posy+1, posx, ' OOO<')
+	stdscr.addstr(posy+2, posx, ' (((')
+	stdscr.refresh()
+	time.sleep(0.2)
+
+
+def main(stdscr):
+	# disable blinking cursor
+	curses.curs_set(False)
+	# clear screen
+	stdscr.clear()
+
+	# main code
+	for i in range(0, 74):
+		drawinsect(stdscr, 10, i)
+
+	# end main code
+	stdscr.getkey()
+
+wrapper(main)
