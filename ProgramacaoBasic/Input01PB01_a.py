0f0c560587ce298476d54af7e003059c713fd6ba
Rodrigo <nobrega@gmail.com>
2015-06-03 22:50:35 +0800

    Programação Basic 01 - Inicio
    # v2.0004
    # 20150603
    New version numbering: 2. for ‘Output’
    0004 for the build

---
+++ ProgramacaoBasic/Input01PB01_a.py
@@ -0,0 +1,23 @@
+# INPUT
+# Programacao BASIC 01
+#
+# Numeros ao acaso
+#
+# fracoes entre 0 e 1
+
+# Implementacao em Python by RN
+
+# v2.0004
+# 20150603
+
+import random
+
+
+def main():
+	a = 1
+	while a > 0:
+		print(random.random())
+
+
+if __name__ == '__main__':
+	main()
