365ae339bad0d51a7cf31ef68ba275863fcbdff5
Rodrigo <nobrega@gmail.com>
2015-06-03 22:58:51 +0800

    Programação Basic 1 - Inteiros
    # v2.0005
    # 20150603

---
+++ ProgramacaoBasic/Input01PB01_b.py
@@ -0,0 +1,23 @@
+# INPUT
+# Programacao BASIC 01
+#
+# Numeros ao acaso
+#
+# inteiros entre 0 e 5
+
+# Implementacao em Python by RN
+
+# v2.0005
+# 20150603
+
+import random
+
+
+def main():
+	a = 1
+	while a > 0:
+		print(random.randint(0, 5))
+
+
+if __name__ == '__main__':
+	main()
