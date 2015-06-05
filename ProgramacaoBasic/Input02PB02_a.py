2dd7202dc7b278e4136076c1904436e4946805d5
Rodrigo <nobrega@gmail.com>
2015-06-05 22:08:14 +0800

    Programação Basic 2 - Pintando o sete
    # v2.0008
    # 20150605
    in Pillow

--- ProgramacaoBasic/Input02PB02_a.py
+++ ProgramacaoBasic/Input02PB02_a.py
@@ -13,20 +13,38 @@
 # https://pillow.readthedocs.org/handbook/concepts.html#concept-modes
 # http://www.w3schools.com/html/html_colornames.asp
 
-# v2.0006
+# v2.0008
 # 20150605
 
-from PIL import Image
+from PIL import Image, ImageDraw
+import random
+
 
 class Tela(object):
-	def __init__(self):
-		self.tela = Image.new('RGB', (640,320), 'Black')
-		self.tela.show()
+	def __init__(self, width, height):
+		self.colors = ['Black', 'Lime', 'Yellow', 'Blue', 'Red', 'White', 'Cyan', 'Magenta', 'Orange']
+		self.tela = Image.new('RGB', (width, height), 'Black')
+		self.desenho = ImageDraw.Draw(self.tela)
+		#self.tela.show()
 
+	def bloco(self, x, y, color):
+		self.desenho.rectangle([(x, y), (x+10, y+10)], color)
+		#self.tela.show()
+
+	def loop(self):
+		for n in range(0,630,10):
+			m = random.randint(0, 31) * 10
+			c = self.colors[random.randint(0,8)]
+			self.bloco(n, m, c)
 
 
 def main():
-	Tela()
+	a = Tela(640, 320)
+	#a.bloco(100, 100, a.colors[1])
+	for i in range(0,50):
+		a.loop()
+	a.tela.show()
+
 
 
 if __name__ == '__main__':
