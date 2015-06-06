fbc72c7cefa719fa339e6d26051fcb8d1c869be0
Rodrigo <nobrega@gmail.com>
2015-06-06 14:47:30 +0800

    Programação Basic 2, Pôr-do-sol
    # v2.0010
    # 20150606
    legal

--- ProgramacaoBasic/Input02PB02_b.py
+++ ProgramacaoBasic/Input02PB02_b.py
@@ -13,7 +13,7 @@
 # https://pillow.readthedocs.org/handbook/concepts.html#concept-modes
 # http://www.w3schools.com/html/html_colornames.asp
 
-# v2.0009
+# v2.0010
 # 20150606
 
 from PIL import Image, ImageDraw
@@ -25,27 +25,27 @@ class Screen(object):
 		self.colors = ['Black', 'Lime', 'Yellow', 'Blue', 'Red', 'White', 'Cyan', 'Magenta', 'Orange']
 		self.screen = Image.new('RGB', (width, height), 'Black')
 		self.draw = ImageDraw.Draw(self.screen)
-		#self.screen.show()
-
-	# def square(self, x, y, color):
-	# 	self.draw.rectangle([(x, y), (x+10, y+10)], color)
-	# 	#self.screen.show()
-
-	# def loop(self):
-	# 	for n in range(0,630,10):
-	# 		m = random.randint(0, 31) * 10
-	# 		c = self.colors[random.randint(0,8)]
-	# 		self.square(n, m, c)
 
 
 class Sunset(object):
 	def __init__(self, tela):
 		self.sun(tela)
+		self.horizon(tela)
 
 	def sun(self, tela):
 		for n in range(0, 80):
 			tela.draw.line([(320, 160), (640-random.randint(0,640), 160-random.randint(0,160))]
 				, tela.colors[2])
+
+	def horizon(self, tela):
+		for n in range(160, 320, 25):
+			tela.draw.line([(320, 160), (0, n)]
+				, tela.colors[4])
+			tela.draw.line([(320, 160), (640, n)]
+				, tela.colors[4])
+		for n in range(0, 640, 20):
+			tela.draw.line([(320, 160), (n, 320)]
+				, tela.colors[4])
 
 
 def main():
