d56463459c93a8c22dfa9648ad1c942336821425
Rodrigo <nobrega@gmail.com>
2015-06-11 22:08:42 +0800

    Código de Máquina 3, Bin Dec Hex
    # v2.0023
    # 20150611
    Classes defined

--- CodigoDeMaquina/Input03CM03_a.py
+++ CodigoDeMaquina/Input03CM03_a.py
@@ -5,22 +5,57 @@
 
 # Implementação em Python by RN
 
-# v2.0022
-# 20150610
+# v2.0023
+# 20150611
 
 import math
 import Input02CM02_a as conv
+import curses
+from curses import wrapper
 
 
-def main():
-    a = input('Enter Base (up to 36) : ')
-    b = input('Enter Number : ')
-    if int(a) <= 36:
-        x = conv.ConvertBaseToDec(a, b)
-        y = conv.ConvertDecToBase(a, x.decimal)
-        x.verbose()
-        y.verbose()
+class BinaryNumber(object):
+    def __init__(self):
+        pass
+
+
+class DecimalNumber(object):
+    def __init__(self):
+        pass
+
+
+class HexNumber(object):
+    def __init__(self):
+        pass
+
+
+class Screen(object):
+    def __init__(self, stdscr):
+        stdscr.addstr(2, 1, '============================================================')
+        stdscr.addstr(3, 1, '==                      BIN , DEC, HEX                    ==')
+        stdscr.addstr(4, 1, '============================================================')
+
+
+def main(stdscr):
+    # disable blinking cursor
+    curses.curs_set(False)
+    # clear screen
+    stdscr.clear()
+
+    # main code
+    Screen(stdscr)
+    # a = input('Enter Base (up to 36) : ')
+    # b = input('Enter Number : ')
+    # if int(a) <= 36:
+    #     x = conv.ConvertBaseToDec(a, b)
+    #     y = conv.ConvertDecToBase(a, x.decimal)
+    #     x.verbose()
+    #     y.verbose()
+
+    # end main code
+    stdscr.getkey()
+
 
 
 if __name__ == '__main__':
-    main()
+    wrapper(main)
