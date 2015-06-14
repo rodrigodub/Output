fb3903a2fafc7242e2b256392f73e92a8e03f9be
Rodrigo <nobrega@gmail.com>
2015-06-14 22:35:08 +0800

    Programação de Jogos 4, Campo Minado/Movimento
    # v2.0033
    # 20150614
    Movement OK

--- ProgramacaoDeJogos/Input04PJ04_a.py
+++ ProgramacaoDeJogos/Input04PJ04_a.py
@@ -5,7 +5,7 @@
 
 # Implementação em Python by RN
 
-# v2.0032
+# v2.0033
 # 20150614
 
 # import math
@@ -28,6 +28,19 @@ class Tank(object):
     def draw(self, stdscr):
         stdscr.addstr(self.lastcoord[1], self.lastcoord[0], self.clean)
         stdscr.addstr(self.coord[1], self.coord[0], self.tank)
+        #stdscr.refresh()
+
+
+class Move(object):
+    def __init__(self, tank, key):
+        if key == 'KEY_UP' and tank.coord[1] > 0:
+            tank.setPos(tank.coord[0], tank.coord[1]-1)
+        elif key == 'KEY_DOWN' and tank.coord[1] < 47:
+            tank.setPos(tank.coord[0], tank.coord[1]+1)
+        elif key == 'KEY_LEFT' and tank.coord[0] > 0:
+            tank.setPos(tank.coord[0]-1, tank.coord[1])
+        elif key == 'KEY_RIGHT' and tank.coord[0] < 79:
+            tank.setPos(tank.coord[0]+1, tank.coord[1])
 
 
 def main(stdscr):
@@ -38,9 +51,14 @@ def main(stdscr):
 
     # main code
     a = Tank(stdscr)
+    e = ''
+    while e != 'x':
+        e = stdscr.getkey()
+        Move(a, e)
+        a.draw(stdscr)
 
     # end main code
-    stdscr.getkey()
+    #stdscr.getkey()
 
 
 if __name__ == '__main__':
