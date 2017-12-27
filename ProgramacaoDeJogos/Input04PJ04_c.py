02bd9094ecaec850d43e7a88928a5f2a3f4dbb02
Rodrigo <nobrega@gmail.com>
2017-12-27 22:07:49 +0800

    PJ4: screen with background OK
    # v2.062
    # 20171227

--- ProgramacaoDeJogos/Input04PJ04_c.py
+++ ProgramacaoDeJogos/Input04PJ04_c.py
@@ -6,7 +6,7 @@
 # Usage:
 # >python3 Input04PJ04_c.py
 #
-# v2.061
+# v2.062
 # 20171227
 #################################################
 __author__ = 'Rodrigo Nobrega'
@@ -17,13 +17,23 @@ import sys, pygame
 import random
 
 
+# background
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+
+
 # define screen
 def setscreen():
     size = (640, 480)
-    backgroundimage = './images/desert640.png'
+    backgroundimage = 'images/desert640.png'
     background = pygame.image.load(backgroundimage)
     screen = pygame.display.set_mode(size)
-    screen.blit(background, size)
+    # screen.fill([120,30,0])
+    screen.blit(background, (0,0))
     return screen
 
 
@@ -39,7 +49,8 @@ def eventloop():
 def main():
     print('Main')
     pygame.init()
-    setscreen()
+    screen = setscreen()
+    # bg = Background()
     eventloop()
 
 
