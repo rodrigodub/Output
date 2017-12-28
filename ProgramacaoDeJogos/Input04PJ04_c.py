95a0bea8b4ac17fc4ed1f24c3de83ca088d1b2f0
Rodrigo <nobrega@gmail.com>
2017-12-28 22:23:27 +0800

    PJ4: code cleaned and commented, so far
    # v2.072
    # 20171228
    
    Next: the paratroopers

--- ProgramacaoDeJogos/Input04PJ04_c.py
+++ ProgramacaoDeJogos/Input04PJ04_c.py
@@ -6,7 +6,7 @@
 # Usage:
 # > python3 Input04PJ04_c.py
 #
-# v2.071
+# v2.072
 # 20171228
 #################################################
 __author__ = 'Rodrigo Nobrega'
@@ -17,53 +17,49 @@ import os, sys, pygame
 import random
 
 
-# load image
+# load image function
 def load_image(file):
     path = os.path.join('images', file)
     return pygame.image.load(path).convert_alpha()
 
 
-# background
-# class Background(object):
-#     def __init__(self, image_file):
-#         # pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
-#         self.image = load_image(image_file)
-#         self.rect = self.image.get_rect()
-#         self.rect.left, self.rect.top = 0, 0
-
-
 # screen
-class Setscreen(object):
+class Setupscreen(object):
+    """Starts a screen and displays background"""
     def __init__(self, image_file):
+        # physical parameters
         self.size = (640, 480)
-        self.bgcolor = [90, 230, 90]
+        self.bgcolour = [90, 230, 90]
+        # the canvas
         self.area = pygame.display.set_mode(self.size)
+        # background image and its enclosing rectangle
         self.image = load_image(image_file)
         self.rect = self.image.get_rect()
-        #self.show(bg)
-        self.area.fill(self.bgcolor)
+        # show image
+        self.show()
+
+    def show(self):
+        # fill screen with solid colour
+        self.area.fill(self.bgcolour)
+        # blit the image
         self.area.blit(self.image, (0, 0))
-            
-    # def show(self, bg):
-    #     # screen = pygame.display.set_mode(self.size)
-    #     #self.area.fill(self.bgcolor)
-    #     #self.area.blit(Background(bg).image, (0, 0))
-    #     # return screen
-    #     pass
+
 
 # tank
 class Tank(object):
+    """Creates the player's tank and controls how it moves"""
     def __init__(self, x, y):
         self.image = load_image('tank4.png')
         self.pos = self.image.get_rect().move(x, y)
 
     def move(self):
+        # movement rules
         self.pos.left += 2
         
 
 # tankmovement
 
-# skydiver
+# paratrooper
 
 # skydmovement
 
@@ -71,22 +67,30 @@ class Tank(object):
 # event loop
 def eventloop(scr, tnk):
     while 1:
+        # quit gracefully
         for event in pygame.event.get():
             if event.type == pygame.QUIT: 
                 sys.exit()
+        # blit the background to erase the last tank position
         scr.area.blit(scr.image, tnk.pos, tnk.pos)
+        # move tank
         tnk.move()
+        # blit tank in new position
         scr.area.blit(tnk.image, tnk.pos)
+        # refresh display
         pygame.display.flip()
 
 
 # main routine
 def main():
     print('Main')
+    # start Pygame
     pygame.init()
-    screen = Setscreen('desert640.png')
-    thetank = Tank(50, 240)
-    #screen.area.blit(thetank.image, thetank.pos)
+    # start the display
+    screen = Setupscreen('desert640.png')
+    # creates tank
+    thetank = Tank(50, 300)
+    # start the event loop
     eventloop(screen, thetank)
 
 
