68814ddf2c00c10ef82f3dafd3ac64932f5931f0
Rodrigo <nobrega@gmail.com>
2018-01-21 22:12:24 +0800

    PB6: Service Station OK
    v2.099

--- ProgramacaoBasic/Input05PB06_b.py
+++ ProgramacaoBasic/Input05PB06_b.py
@@ -6,10 +6,16 @@
 # Usage:
 # > python3 Input05PB06_b.py
 #
-# v2.098
+# v2.099
 # 20180121
 #################################################
 __author__ = 'Rodrigo Nobrega'
+
+# global variables
+# prices in R$, Jan 2018
+ETH = 3.199
+GAS = 4.299
+DIE = 3.699
 
 
 # import
@@ -65,24 +71,61 @@ class Choice(object):
     def __init__(self):
         self.option = ''
         self.type = ['Ethanol', 'Gasoline', 'Diesel']
+        self.coords = [262, 255, 275]
+        self.position = 0
+        self.price = 0
 
     def select(self):
         keys = pygame.key.get_pressed()
         if keys[K_e]:
             self.option = self.type[0]
+            self.position = self.coords[0]
+            self.price = ETH
         if keys[K_g]:
             self.option = self.type[1]
+            self.position = self.coords[1]
+            self.price = GAS
         if keys[K_d]:
             self.option = self.type[2]
-
+            self.position = self.coords[2]
+            self.price = DIE
 
 
 # tank fill
+class Fill(object):
+    """Fill the tank"""
+    def __init__(self):
+        self.volume = 0
+
+    def press(self):
+        keys = pygame.key.get_pressed()
+        if self.volume <= 70:
+            if keys[K_SPACE]:
+                self.volume += 0.01
+
+
+# event loop MAIN
+def eventloopmain(scr, fnt1, fnt2, opt, pmp):
+    # arguments: scr=screen, fntn=font, opt=fuel option, pmp=pump
+    a = 1
+    while a == 1:
+        # quit gracefully
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or pygame.key.get_pressed()[K_q]:
+                sys.exit()
+        # switch between event loops
+        if opt.option == '':
+            # choose fuel
+            eventloop1(scr, fnt1, opt)
+        else:
+            # fill tank
+            eventloop2(scr, fnt1, fnt2, opt, pmp)
 
 
 # event loop CHOICE
 def eventloop1(scr, fnt, opt):
     # arguments: scr=screen, fnt=font, opt=fuel option
+    scr.area.blit(scr.image, (0, 0))
     a = 1
     while a == 1:
         # quit gracefully
@@ -99,6 +142,7 @@ def eventloop1(scr, fnt, opt):
         scr.area.blit(writetext(fnt, opt.type[1][0], (180, 100, 100)), (285, 220))
         scr.area.blit(writetext(fnt, opt.type[2], (0, 0, 0)), (285, 240))
         scr.area.blit(writetext(fnt, opt.type[2][0], (180, 100, 100)), (285, 240))
+        scr.area.blit(writetext(fnt, 'Press Q to quit', (100, 100, 100)), (255, 290))
         # check option
         opt.select()
         if opt.option != '':
@@ -108,10 +152,10 @@ def eventloop1(scr, fnt, opt):
 
 
 # event loop TANK FILL
-def eventloop2(scr, fnt1, fnt2, opt):
-    # arguments: scr=screen, fntn=font, opt=fuel option
+def eventloop2(scr, fnt1, fnt2, opt, pmp):
+    # arguments: scr=screen, fntn=font, opt=fuel option, pmp=pump
     # clear screen (part of it)
-    scr.area.blit(scr.image, (230, 145, 180, 115), (230, 145, 180, 115))
+    scr.area.blit(scr.image, (230, 145, 180, 165), (230, 145, 180, 165))
     a = 1
     while a == 1:
         # quit gracefully
@@ -119,8 +163,26 @@ def eventloop2(scr, fnt1, fnt2, opt):
             if event.type == pygame.QUIT or pygame.key.get_pressed()[K_q]:
                 sys.exit()
         # write text
-        scr.area.blit(writetext(fnt1, 'Press N to fill', (100, 100, 100)), (260, 150))
-        scr.area.blit(writetext(fnt2, '{}'.format(opt.option), (180, 100, 100)), (260, 170))
+        scr.area.blit(writetext(fnt1, 'Press SPACE to fill, P to pay and finish', (100, 100, 100)), (160, 150))
+        scr.area.blit(writetext(fnt2, '{}'.format(opt.option), (180, 100, 100)), (opt.position, 170))
+        scr.area.blit(writetext(fnt1, 'R$ {} / L'.format(opt.price), (100, 100, 100)), (270, 212))
+        # volume and price
+        scr.area.blit(writetext(fnt1, 'Litres', (100, 100, 100)), (150, 290))
+        scr.area.blit(writetext(fnt1, 'R$', (100, 100, 100)), (470, 290))
+        # fill tank
+        pmp.press()
+        # number of litres
+        scr.area.blit(scr.image, (100, 310, 140, 40), (100, 310, 140, 40))
+        scr.area.blit(writetext(fnt2, '{}'.format(round(pmp.volume, 3)), (0, 0, 0)), (130, 310))
+        # value in Real
+        scr.area.blit(scr.image, (410, 310, 140, 40), (410, 310, 140, 40))
+        scr.area.blit(writetext(fnt2, '{}'.format(round(pmp.volume * opt.price, 2)), (0, 0, 0)), (425, 310))
+        # tests payment
+        if pygame.key.get_pressed()[K_p]:
+            # initialise volume, clear option, exist loop
+            pmp.volume = 0
+            opt.option = ''
+            a = 0
         # refresh display
         pygame.display.update()
 
@@ -134,14 +196,13 @@ def main():
     font1 = pygame.font.Font('./fonts/Chicago Normal.ttf', 16)
     font2 = pygame.font.Font('./fonts/Chicago Normal.ttf', 32)
     # start the display
-    # screen = Setupscreen()
     screen = Setupscreen('background green 640x480.png')
-    #create choice
+    # create choice
     option = Choice()
-    # starts the event loop CHOICE
-    eventloop1(screen, font1, option)
-    # starts the event loop TANK FILL
-    eventloop2(screen, font1, font2, option)
+    # turn on pump
+    pump = Fill()
+    # starts the event loop MAIN
+    eventloopmain(screen, font1, font2, option, pump)
 
 
 # execute main
