32a1f3198c27a5b87d4cbe6764f798c6157543ab
Rodrigo <nobrega@gmail.com>
2018-01-21 15:43:06 +0800

    PB6: variables
    # v2.093
    # 20180120

---
+++ ProgramacaoBasic/Input05PB06_a.py
@@ -0,0 +1,19 @@
+#################################################
+#                   INPUT
+# Programação Basic 6
+# O que são variáveis
+#
+# Usage:
+# > python3 Input05PB06_a.py
+#
+# v2.093
+# 20180120
+#################################################
+# coding: utf-8
+
+x = 0
+    
+while True:
+    for t in range(0, 10):
+        print(x)
+        x += 1
