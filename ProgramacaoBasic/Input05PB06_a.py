d249a5191048bf9431faba148e0d696a22c0f047
Rodrigo <nobrega@gmail.com>
2018-01-21 16:26:25 +0800

    PB6: variables 1 OK
    v2.095

--- ProgramacaoBasic/Input05PB06_a.py
+++ ProgramacaoBasic/Input05PB06_a.py
@@ -6,7 +6,7 @@
 # Usage:
 # > python3 Input05PB06_a.py
 #
-# v2.094
+# v2.095
 # 20180121
 #################################################
 
@@ -16,8 +16,8 @@ import time
 def main():
     x = 0
     while True:
-        print('{} '.format(x))
-        time.sleep(.05)
+        print('{} '.format(x), end='', flush=True)
+        time.sleep(.025)
         x += 1
 
 
