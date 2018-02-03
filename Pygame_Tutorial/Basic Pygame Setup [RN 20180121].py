48a21124e47c8933ac4e997a5c02fe42a805b8ae
Rodrigo <nobrega@gmail.com>
2018-02-03 20:29:37 +0800

    Updated Basic Setup
    # v2.125
    # 20180203

--- Pygame_Tutorial/Basic Pygame Setup [RN 20180121].py
+++ Pygame_Tutorial/Basic Pygame Setup [RN 20180121].py
@@ -6,8 +6,8 @@
 # Usage:
 # > python3 name.py
 #
-# v2.108
-# 20180128
+# v2.125
+# 20180203
 #################################################
 __author__ = 'Rodrigo Nobrega'
 
@@ -16,7 +16,7 @@ __author__ = 'Rodrigo Nobrega'
 import os, sys, pygame
 from pygame.locals import *
 # import random
-# import time
+# import math
 
 
 # load image function
@@ -33,14 +33,15 @@ def writetext(font, text, colour):
 
 
 # screen
-class Setupscreen(object):
+class Screen(object):
     """Starts a screen and displays background"""
     def __init__(self, image_file=None):
         # physical parameters
-        self.size = (640, 480)
+        self.size = (1024, 576)
         self.bgcolour = [230, 230, 230]
         # the canvas
-        self.area = pygame.display.set_mode(self.size)
+        self.display = pygame.display.set_mode(self.size)
+        self.title = pygame.display.set_caption('Pygame app')
         # background image and its enclosing rectangle
         if image_file:
             self.image = load_image(image_file)
@@ -53,10 +54,10 @@ class Setupscreen(object):
 
     def show(self):
         # fill screen with solid colour
-        self.area.fill(self.bgcolour)
+        self.display.fill(self.bgcolour)
         # blit background image
         if self.image != '':
-            self.area.blit(self.image, (0, 0))
+            self.display.blit(self.image, (0, 0))
 
 
 # event loop
@@ -71,24 +72,24 @@ def eventloop(scr, fnt, clk, sco):
         # measure time
         clk.tick(60)
         # write text
-        # scr.area.blit(scr.image, (120, 5, 50, 30), (120, 5, 50, 30))
-        scr.area.blit(writetext(fnt, 'OK: {}'.format(sco), (100, 100, 100)), (10, 10))
+        # scr.display.blit(scr.image, (120, 5, 50, 30), (120, 5, 50, 30))
+        scr.display.blit(writetext(fnt, 'OK: {}'.format(sco), (100, 100, 100)), (10, 10))
         # refresh display
         pygame.display.flip()
 
 
 # main routine
 def main():
-    print('\n ::: Input 05 - Variables :::\n\n       Press [Q] to quit.\n')
+    print('\n ::: Basic Initial Setup :::\n\n       Press [Q] to quit.\n')
     # start Pygame
     pygame.init()
     pygame.mixer.init()
-    font1 = pygame.font.Font('./fonts/Chicago Normal.ttf', 16)
+    font1 = pygame.font.Font('Chicago Normal.ttf', 16)
     clock = pygame.time.Clock()
     score = 0
     # start the display
-    screen = Setupscreen()
-    #screen = Setupscreen('background green 640x480.png')
+    screen = Screen()
+    #screen = Screen('background green 640x480.png')
     # start the event loop with tank moving right
     eventloop(screen, font1, clock, score)
 
