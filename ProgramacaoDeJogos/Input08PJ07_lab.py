93802f7ae40d5a9d6c5a2abebe6954f4d535edb3
Rodrigo <nobrega@gmail.com>
2018-02-11 21:16:28 +0800

    PJ7:lab, it seems all exceptions have been catched
    # v2.143
    # 20180211

--- ProgramacaoDeJogos/Input08PJ07_lab.py
+++ ProgramacaoDeJogos/Input08PJ07_lab.py
@@ -6,7 +6,7 @@
 # Usage:
 # > python3 Input08PJ07.py
 #
-# v2.142
+# v2.143
 # 20180211
 #################################################
 __author__ = 'Rodrigo Nobrega'
@@ -23,50 +23,54 @@ class Maze(object):
         self.ny = int(576/self.bs)
         # dim p - p is the labirinth
         self.p = np.zeros((self.nx, self.ny), dtype=int)
-        # self.p = np.zeros((self.nx + 5, self.ny + 5), dtype=int)
         # draw border
         self.border()
         # coordinates
         self.x = 1
         self.y = 1
-        #self.x = 2
-        #self.y = 2
         # last coordinates
         self.lx = 1
         self.ly = 1
-        #self.lx = 2
-        #self.ly = 2
         # maze positions
         self.j = 0
         self.g = 0
         # create maze
-        self.createmaze1()
-
+        try:
+            self.createmaze1()
+        except RecursionError:
+            print('Recursion Error')
 
     def border(self):
+        # horizontal borders
         for j in range(0, self.nx):
             self.p[j, self.ny-1] = 6
             self.p[j, 0] = 6
+        # vertical borders
         for j in range(0, self.ny):
             self.p[0, j] = 6
             self.p[self.nx-1, j] = 6
 
     def createmaze1(self):
+        # direction
         self.j = random.randint(0, 3)
         self.g = self.j
         self.createmaze2()
 
     def createmaze2(self):
+        # increment
         self.y = self.ly + 2 * ((self.j == 0) - (self.j == 2))
         self.x = self.lx + 2 * ((self.j == 3) - (self.j == 1))
-        if self.x > self.nx-1:
+        # prevent coord indexes to be greater than array size
+        if self.x >= self.nx-1:
             self.x -= self.nx
-        if self.y > self.ny-1:
+        if self.y >= self.ny-1:
             self.y -= self.ny
+        # but still need to catch exception
         try:
             self.p[self.x, self.y]
         except IndexError:
             self.createmaze3()
+        # main routine, change the following two blocks
         if self.p[self.x, self.y] == 0:
             self.p[self.x, self.y] = self.j+1
             self.p[int((self.x + self.lx) / 2), int((self.y + self.ly) / 2)] = 5
@@ -77,6 +81,7 @@ class Maze(object):
             self.createmaze3()
 
     def createmaze3(self):
+        # choose new direction
         self.j = (self.j + 1) % 4
         if self.j != self.g:
             self.createmaze2()
@@ -84,6 +89,7 @@ class Maze(object):
             self.createmaze4()
 
     def createmaze4(self):
+        # have to catch de index exception for the last postions
         try:
             self.j = self.p[self.lx, self.ly] - 1
             self.p[self.lx, self.ly] = 5
