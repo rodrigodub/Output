5b8f97392aee3bfe454c11b84d162c415086a76e
Rodrigo <nobrega@gmail.com>
2019-02-12 21:17:07 +0800

    PJ9: starting an Adventure
    v2.300

---
+++ ProgramacaoDeJogos/I11PJ9_Adventure.py
@@ -0,0 +1,114 @@
+#################################################
+#                   INPUT 11
+# Programacao de Jogos 9
+# The Adventure
+#
+# UK : Input 9-13
+# Wireframe #6
+# How to write a Text Adventure:
+# https://letstalkdata.com/2014/08/how-to-write-a-text-adventure-in-python/
+#
+# Usage:
+# > python3 I11PJ9_Adventure.py
+#
+# v2.300
+# 20190211
+#################################################
+__author__ = 'Rodrigo Nobrega'
+
+
+# # import
+# import arcade
+#
+#
+# # Global variables
+# SCREEN_WIDTH = 1024
+# SCREEN_HEIGHT = 576
+#
+#
+# class MyGame(arcade.Window):
+#     """
+#     Main application class.
+#
+#     NOTE: Go ahead and delete the methods you don't need.
+#     If you do need a method, delete the 'pass' and replace it
+#     with your own code. Don't leave 'pass' in this program.
+#     """
+#
+#     def __init__(self, width, height):
+#         super().__init__(width, height, title="Treasure Hunt")
+#
+#         arcade.set_background_color(arcade.color.AMAZON)
+#
+#         # If you have sprite lists, you should create them here,
+#         # and set them to None
+#
+#     def setup(self):
+#         # Create your sprites and sprite lists here
+#         pass
+#
+#     def on_draw(self):
+#         """
+#         Render the screen.
+#         """
+#
+#         # This command should happen before we start drawing. It will clear
+#         # the screen to the background color, and erase what we drew last frame.
+#         arcade.start_render()
+#
+#         # Call draw() on all your sprite lists below
+#
+#     def update(self, delta_time):
+#         """
+#         All the logic to move, and the game logic goes here.
+#         Normally, you'll call update() on the sprite lists that
+#         need it.
+#         """
+#         pass
+#
+#     def on_key_press(self, key, key_modifiers):
+#         """
+#         Called whenever a key on the keyboard is pressed.
+#
+#         For a full list of keys, see:
+#         http://arcade.academy/arcade.key.html
+#         """
+#         pass
+#
+#     def on_key_release(self, key, key_modifiers):
+#         """
+#         Called whenever the user lets off a previously pressed key.
+#         """
+#         pass
+#
+#     def on_mouse_motion(self, x, y, delta_x, delta_y):
+#         """
+#         Called whenever the mouse moves.
+#         """
+#         pass
+#
+#     def on_mouse_press(self, x, y, button, key_modifiers):
+#         """
+#         Called when the user presses a mouse button.
+#         """
+#         pass
+#
+#     def on_mouse_release(self, x, y, button, key_modifiers):
+#         """
+#         Called when a user releases a mouse button.
+#         """
+#         pass
+
+
+# main routine
+def main():
+    """ Main method """
+    # game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    # game.setup()
+    # arcade.run()
+    pass
+
+
+# execute main
+if __name__ == "__main__":
+    main()
