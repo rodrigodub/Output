4638e51d7276700529c2587b8cf960b0076778ab
Rodrigo <nobrega@gmail.com>
2020-09-27 18:16:34 +0800

    v.3.006 / Spawn v0.01 First commit of Spawn. Not working

---
+++ spawn/README.md
@@ -0,0 +1,34 @@
+# Spawn
+
+My own implementation of **Conway's Game of Life**
+
+##### Author: Rodrigo Nobrega
+##### Created on: 2020.09.27
+##### Last modified: 2020.09.27
+
+### Version 0.01
+###### Output v.3.006
+
+---
+
+## References
+
+- Implemented with [Python Arcade](https://arcade.academy/) library
+- Python Arcade [documentation on RealPython](https://realpython.com/arcade-python-game-framework/)
+- [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life) Wikipedia article
+- Game of Life [implemented in Javascript by Coding Train](https://www.youtube.com/watch?v=FWSR_7kZuYg&t=1586s), and in [Python/Pygame by DevDungeon](https://www.youtube.com/watch?v=VNAU7HH4QRw)
+- [Epic video](https://www.youtube.com/watch?v=C2vgICfQawE)
+
+---
+
+## A few requirements
+
+- Implement the Game of Life rules
+- Define board size
+- Zoom in/out
+- Press S to start/stop the generations
+- Press R to randomize
+- Click cell to alter its state
+- Ability to create known patterns at a given position
+
+
---
+++ spawn/spawn.py
@@ -0,0 +1,128 @@
+#################################################
+#                   Spawn
+# Conway's Game of Life  in Python Arcade
+#################################################
+__author__ = 'Rodrigo Nobrega'
+__version__ = 0.01
+
+
+# Imports
+import arcade
+import numpy as np
+
+
+# Constants
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 576
+SCREEN_TITLE = 'Spawn - Game of Life'
+# BGCOLOUR = (220, 220, 220)  # GAINSBORO
+BGCOLOUR = (227, 218, 201)	 # BONE
+RESOLUTION = 10
+
+
+# Classes
+class Spawn(arcade.Window):
+    """
+    Main application class.
+
+    NOTE: Go ahead and delete the methods you don't need.
+    If you do need a method, delete the 'pass' and replace it
+    with your own code. Don't leave 'pass' in this program.
+    """
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+        # arcade.set_background_color(arcade.color.AMAZON)
+        arcade.set_background_color(BGCOLOUR)
+
+        # If you have sprite lists, you should create them here,
+        # and set them to None
+        self.board = None
+
+    def setup(self):
+        # Create your sprites and sprite lists here
+        # 1. define random initial state
+        self.board = np.random.randint(2,
+                                       size=(int(SCREEN_HEIGHT / RESOLUTION),
+                                             int(SCREEN_WIDTH / RESOLUTION)))
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command should happen before we start drawing. It will clear
+        # the screen to the background color, and erase what we drew last frame.
+        arcade.start_render()
+
+        # Call draw() on all your sprite lists below
+
+        for i in self.board.shape[0]:
+            for j in self.board.shape[1]:
+                if self.board[i, j] == 1:
+                    arcade.draw_xywh_rectangle_filled(j * RESOLUTION, i * RESOLUTION,
+                                                      RESOLUTION, RESOLUTION, (0, 0, 0))
+                else:
+                    arcade.draw_xywh_rectangle_outline(j * RESOLUTION, i * RESOLUTION,
+                                                      RESOLUTION, RESOLUTION, (0, 0, 0))
+
+    def update(self, delta_time):
+        """
+        All the logic to move, and the game logic goes here.
+        Normally, you'll call update() on the sprite lists that
+        need it.
+        """
+        pass
+
+    def on_key_press(self, key, key_modifiers):
+        """
+        Called whenever a key on the keyboard is pressed.
+
+        For a full list of keys, see:
+        http://arcade.academy/arcade.key.html
+        """
+        pass
+
+    def on_key_release(self, key, key_modifiers):
+        """
+        Called whenever the user lets off a previously pressed key.
+        """
+        pass
+
+    def on_mouse_motion(self, x, y, delta_x, delta_y):
+        """
+        Called whenever the mouse moves.
+        """
+        pass
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+        pass
+
+    def on_mouse_release(self, x, y, button, key_modifiers):
+        """
+        Called when a user releases a mouse button.
+        """
+        pass
+
+
+class Board(object):
+    def __init__(self, width, height, size):
+        # define random initial state
+        self.grid = np.random.randint(2, size=(int(height/size), int(width/size)))
+
+
+# main routine
+def main():
+    """ Main method """
+    game = Spawn(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game.setup()
+    arcade.run()
+
+
+# execute main
+if __name__ == "__main__":
+    main()
